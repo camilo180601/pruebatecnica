be9a8f414a456641f2fa02fcd124b77d9e15fa69
Camilo Lopez <camilo06180401@gmail.com>
2024-12-04 00:14:31 -0500

    fix url to local

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,11 @@ import './style.css';
 import App from './App.vue';
 import axios from 'axios';
 
-// Crea la aplicación Vue
-
 // Configura Axios como una propiedad global
-axios.defaults.baseURL = 'https://api.allorigins.win/raw?url=https://pruebaback.rf.gd';
-axios.defaults.headers.common['Origin'] = 'https://fronttest-chi.vercel.app';
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.headers.common['Origin'] = 'http://localhost:5173';
 
+// Crea la aplicación Vue
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.mount('#app');
